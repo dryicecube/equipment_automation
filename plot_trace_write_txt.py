6c9bd341d85838c7df70d7173eae2f26964936bc
dryicecube <ashutosh.lunabotics@gmail.com>
2022-09-21 12:14:45 +0530

    added AutoSet

--- plot_trace_write_txt.py
+++ plot_trace_write_txt.py
@@ -18,7 +18,7 @@ from datetime import datetime
 rth = None
 try:
     
-    rth = RsInstrument('TCPIP::172.16.0.92::INSTR', True, False)
+    rth = RsInstrument('TCPIP::172.16.0.82::INSTR', True, False)		#localhost ip address
     #rth = RsInstrument('TCPIP::169.254.253.36::INSTR', True, False)
     rth.visa_timeout = 10000  # Timeout for VISA Read Operations
     rth.opc_timeout = 10000  # Timeout for opc-synchronised operations
@@ -34,38 +34,48 @@ rth.clear_status()
 rth.reset()
 
 rth.write_str("CHAN:TYPE HRES")     # Switching high res
-rth.write_str("CHAN1:STAT OFF")     # Switch Channel 1 OFF
-rth.write_str("CHAN2:STAT ON")      # Switch Channel 2 ON
+rth.write_str("CHAN1:STAT ON")     # Switch Channel 1 OFF
+#rth.write_str("CHAN2:STAT ON")      # Switch Channel 2 ON
 
-tmscale= 1E-8                              #enter the timescae
+tmscale= 5E-9                              #enter the timescae
+
+
+#x=np.arange(0,12*tmscale,0.01)
+#x=np.linspace(0,12*tmscale,1200)
+
+
+rth.write_str("AUT")
 timescale = 'TIM:SCAL '+ str(tmscale)
 rth.write_str((timescale))          #timescale
+# rth.write_str("CHAN1:SCAL .002")     #scale
+# rth.write_str("CHAN1:POS -5")        #position 
+# #rth.write_str("CHAN1:COUP DCL")    #1Mohm termination resistance
+rth.write_str("CHAN1:COUP DC")      #50ohm termination resistance
 
-
-rth.write_str("CHAN2:SCAL 0.5")     #scale
-rth.write_str("CHAN2:POS 0")        #position 
-#rth.write_str("CHAN1:COUP DCL")    #1Mohm termination resistance
-rth.write_str("CHAN2:COUP DC")      #50ohm termination resistance
-rth.write_str("TRIG:A:SOUR CH2")    #trigger channel
 rth.write_str("TRIG:A:TYPE RIS")    #rising edge rigger
-#rth.write_str("TRIG:A:EDGE:SLOP NEG")
+rth.write_str("TRIG:A:LEV1 0")
+rth.write_str("TRIG:A:SOUR CH1")    #trigger channel
+rth.write_str("TRIG:A:EDGE:SLOP POS")
+
+
+
 
 
 start = time()
 rth.write_str("FORM:DATA ASC")
 rth.data_chunk_size = 1000000  # transfer in blocks of 100k bytes (default)
-data_asc = rth.query_bin_or_ascii_float_list("CHAN2:DATA?")
+data_asc = rth.query_bin_or_ascii_float_list("CHAN1:DATA?")
 
 ##writing data in file
 data=np.array(data_asc)
 now = datetime.now()
-f = open('run_file'+str(now), 'a')
+f = open('run_file_'+str(tmscale)+'_'+str(now), 'a')
 for i in range(len(data)):
     f.write(f"{data[i]}\n")
 print(f'ASCII waveform transfer elapsed time: {time() - start:.3f}sec')
 
 
-plt.figure(2)
+plt.figure(1)
 plt.plot(data_asc)
 plt.title('waveform; Timescale:'+str(tmscale)) 
 plt.grid()
